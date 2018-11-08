bce5badedba6a83c70be65c2a6bff78552a72c1b
Eric Burcham <eburcham@eprod.com>
2018-11-08 11:18:24 -0600

    User service migrations are running against a local database.

--- src/end-to-end-example/UserService.Migrations/Program.cs
+++ src/end-to-end-example/UserService.Migrations/Program.cs
@@ -1,12 +1,80 @@
 ﻿using System;
+using System.Data.SqlClient;
+using System.Reflection;
+using DbUp;
+using DbUp.Engine;
 
 namespace UserService.Migrations
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var csb = new SqlConnectionStringBuilder
+            {
+                ApplicationName = "UserService.Migrations",
+                DataSource = ".",
+                InitialCatalog = "users",
+                MultipleActiveResultSets = false,
+                Password = "yourStrong(!)Password",
+                UserID = "sa"
+            };
+
+            Console.WriteLine(csb.ConnectionString);
+
+            // Get a connection string.
+            if (!TryParseConnectionString(args, out var connectionString))
+            {
+                connectionString = csb.ConnectionString;
+//                Console.ForegroundColor = ConsoleColor.Red;
+//                Console.WriteLine("You must provide a valid SQL server connection string.");
+//                Environment.Exit(-1);
+            }
+
+            // Build and run our upgrader.
+            var result = PerformUpgrade(connectionString);
+
+            if (!result.Successful)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(result.Error);
+                Console.ResetColor();
+                Environment.Exit(-1);
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Database Migrations Succeeded!");
+            Console.ResetColor();
+        }
+
+        private static DatabaseUpgradeResult PerformUpgrade(string connectionString)
+        {
+            EnsureDatabase.For.SqlDatabase(connectionString);
+
+            var upgradeEngine = DeployChanges
+                .To
+                .SqlDatabase(connectionString)
+                .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+                .LogToConsole()
+                .Build();
+
+            return upgradeEngine.PerformUpgrade();
+        }
+
+        private static bool TryParseConnectionString(string[] args, out string connectionString)
+        {
+            try
+            {
+                var connectionStringFromArgs = args[1];
+                var connectionStringBuilder = new SqlConnectionStringBuilder(connectionStringFromArgs);
+                connectionString = connectionStringBuilder.ConnectionString;
+                return true;
+            }
+            catch (Exception)
+            {
+                connectionString = null;
+                return false;
+            }
         }
     }
 }
